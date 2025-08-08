f86a3e63c3755bbf489fa244159a9812f1aca31c
Hassannewcode <privatealkassan@gmail.com>
2025-08-08 21:14:58 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,8 +2,13 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
 */
+import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './src/components/App.jsx';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
